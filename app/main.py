45ab8991e9173d1b34fbd88aabebd8d3b1a5ec8a
Tijmen Verhoef <tijmenmenno@gmail.com>
2020-01-15 15:36:55 +0100

    added json file implementation

--- app/main.py
+++ app/main.py
@@ -17,29 +17,21 @@ if (os.getenv('GITHUB_API_KEY_FILE', None) != None):
 
 githubURL = 'https://api.github.com/graphql'
 requestHeaders = {'Authorization': f'bearer {OAuthKey}'}
-requestData = " \
-{ \
-    \"query\": \"query { \
-        viewer { \
-            repositories(first: 100, affiliations: [OWNER, COLLABORATOR, ORGANIZATION_MEMBER]) { \
-                totalCount \
-                nodes { \
-                    name \
-                    url \
-                    description \
-                    isPrivate \
-                    id \
-                } \
-            } \
-            login \
-            name \
-        } \
-    } \" \
-} \
-"
+
+def getRequestData(filepath):
+    requestFile = open(filepath, 'r')
+    requestData = requestFile.read()
+    requestFile.close()
+
+    return requestData
+
+@app.route('/companies', methods=['GET'])
+def getCompanies():
+    return "Not yet implemented", 200
 
 @app.route('/repositories', methods=['GET'])
-def getAll():
+def getRepositories():
+    requestData = getRequestData('app/repositoryRequest.json')
     response = requests.post(githubURL, data=requestData, headers=requestHeaders)
 
     if response.status_code != 200:
